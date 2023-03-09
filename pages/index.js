34b42760f57c54386bc6f66d4751d64a7f025a67
Jasper <it@belastingadviesgroeprotterdam.nl>
2023-03-09 19:15:43 +0100

    animal changed to ingredients

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [ingredientsInput, setIngredientsInput] = useState("");
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
@@ -14,7 +14,7 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ ingredients: ingredientsInput }),
       });
 
       const data = await response.json();
@@ -22,8 +22,11 @@ export default function Home() {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
 
+      console.log(data.result);
+
       setResult(data.result);
-      setAnimalInput("");
+
+      setIngredientsInput("");
     } catch(error) {
       // Consider implementing your own error handling logic here
       console.error(error);
@@ -34,24 +37,24 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <title>Recepy generator</title>
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3>Enter ingredients</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="ingredients"
+            placeholder="Enter ingredients"
+            value={ingredientsInput}
+            onChange={(e) => setIngredientsInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Generate recepy" />
         </form>
-        <div className={styles.result}>{result}</div>
+        <div className={styles.result}>
+          {result}
+        </div>
       </main>
     </div>
   );
